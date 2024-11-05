0393353d94de8f94791b11e131846610bd29942d
arfa2008 <arfayaseen975@gmail.com>
2024-11-05 12:20:59 +0530

    initial changes

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 function checkPalindrome() {
-    // Get the input string value
     let str = document.getElementById("inputString").value;
-
-    // Remove non-alphanumeric characters and convert to lowercase
     let cleanedStr = cleanString(str);
-
-    // Reverse the cleaned string
     let reversedStr = reverseString(cleanedStr);
-
-    // Show result
     displayResult(str, cleanedStr, reversedStr);
 }
 
-// Function to clean the string (remove spaces and special characters, make lowercase)
 function cleanString(str) {
     return str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 }
 
-// Function to reverse a string
 function reverseString(str) {
     return str.split('').reverse().join('');
 }
 
-// Function to display the result on the webpage
 function displayResult(originalStr, cleanedStr, reversedStr) {
     let resultElement = document.getElementById("result");
-
     if (cleanedStr === reversedStr && cleanedStr !== "") {
         resultElement.innerHTML = `"${originalStr}" is a palindrome.`;
         resultElement.classList.remove("error");
